a81c5b78dbcd745fbad5bc52df0eb54eee7c3699
Ryan-J-Kennedy <56158637+Ryan-J-Kennedy@users.noreply.github.com>
2021-08-24 14:52:15 +0200

    Added basic boat catching mechanics

---
+++ Assets/Scripts/FishBoatController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishBoatController : MonoBehaviour
+{
+    Transform fishingRod;
+    Transform hook;
+    public float fishingRodSpeed;
+    Transform player;
+    public Transform rodPos;
+
+    public bool hooked = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fishingRod = this.gameObject.transform.Find("Fishing rod");
+        hook = fishingRod.gameObject.transform.Find("Hook");
+        player = GameObject.Find("Player").transform;
+    }
+
+    private void FixedUpdate()
+    {
+        Hooked();
+        if (hook.position.y > player.position.y)
+        {
+            fishingRod.transform.position -= new Vector3(0f, fishingRodSpeed, 0f);
+        }
+    }
+
+    public void Hooked()
+    {
+        if (hook.position.y < rodPos.position.y && hooked)
+        {
+            fishingRod.transform.position -= new Vector3(0f, -fishingRodSpeed * 2, 0f);
+        }
+    }
+}
